b854c0ef66f101bd3aab1b7b1b95168e43fe32d6
uuwwweeeekkkkk <ahmadjantoro@gmail.com>
2023-05-19 09:12:53 +0700

    update session logout, biar semua efin ngga berbarengan ketika logout

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-session_destroy();
+// session_destroy();
 include "fungsi/koneksi.php";
 // log
 $queryUser =  mysqli_query($koneksi, "SELECT * from user WHERE username  = '$_SESSION[username_blw]'");
